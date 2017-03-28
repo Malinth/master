aa83e0e6a9975eb3f9ff105a5833355c10fa7dcd
Malinth <malin.thunberg@hotmail.com>
2017-03-28 19:25:50 +0200

    edit api.php and add config.php

--- api.php
+++ api.php
@@ -1,18 +1,6 @@
 <?php
-
+require("config.php");
 session_start();
-header('Access-Control-Allow-Origin: *');
-
-$host = 'localhost';
-$db = 'hyresvarlden';
-$user = 'root';
-$password = '';
-$charset = 'utf8';
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-			  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-			  PDO::ATTR_EMULATE_PREPARES   => false  ];
-$pdo = new PDO($dsn, $user, $password, $options);
 
 $user = $_POST['user'];
 $pwd = $_POST['pwd'];
@@ -20,28 +8,20 @@ $pwd = $_POST['pwd'];
 $sql = "SELECT * FROM `gastinlogg` WHERE epost = :user AND pwd = :pwd";
 $statement = $pdo->prepare($sql);
 $statement->execute(array(':user' => $user, ':pwd' => $pwd));
-
 $row = $statement->fetch(PDO::FETCH_ASSOC);
 
-$resultat = [];
+	$result = [
+		"user" => false,
+		"pwd" => false
+		];
 
 if (!is_null($row['ID'])) {
 	$_SESSION['ID'] = $row['ID'];
-	$resultat = [
+	$result = [
 		"user" => true,
 		"pwd" => true
 	];
 }
 
-else {
 
-	$resultat = [
-		"user" => false,
-		"pwd" => false
-	];
-
-}
-
-echo json_encode($resultat);
-
-?>
+echo json_encode($result);
